6837c1917f8b3d2881dfcd03cd04a913e6762582
Manish kumar <mk772542@gmail.com>
2024-12-06 23:34:08 +0530

    Add/Update files

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use("", mainroute)
 
 // Start the server
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;  // Corrected the environment variable name
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
